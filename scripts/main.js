71a3ad7369cd5baa520ae8e697850653edb11861
Sam Daramroei <samdotmc@gmail.com>
2025-02-27 00:17:59 +0300

    Add mobile menu functionality, update favicon, and optimize CSS styles

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const mobileMenuButton = document.getElementById("mobile-menu-button");
+  const mobileMenu = document.getElementById("mobile-menu");
+  const closeMenuButton = mobileMenu.querySelector("button");
+
+  mobileMenuButton.addEventListener("click", function () {
+    mobileMenu.classList.toggle("hidden");
+  });
+
+  closeMenuButton.addEventListener("click", function () {
+    mobileMenu.classList.add("hidden");
+  });
+
+  document
+    .querySelectorAll('[data-modal-hide="popup-modal"]')
+    .forEach((button) => {
+      button.addEventListener("click", function () {
+        document.getElementById("popup-modal").classList.add("hidden");
+      });
+    });
+
+  document
+    .querySelectorAll('[data-menu-hide="mobile-menu"]')
+    .forEach((button) => {
+      button.addEventListener("click", function () {
+        mobileMenu.classList.add("hidden");
+      });
+    });
+});
